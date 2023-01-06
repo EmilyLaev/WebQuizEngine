0d38fca4eb58dc08c314b149e35504c1538df312
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-06 18:41:39 -0500

    Update CompletedLogServices.java

--- CompletedLogServices.java
+++ CompletedLogServices.java
@@ -1,15 +1,17 @@
 package engine.services;
 
-
 import engine.entity.CompletedLog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+//Interface for interacting with CompletedLog entities.
 public interface CompletedLogServices {
 
+    //Persists the given CompletedLog to the database.
     void saveCompletedLog(CompletedLog completedLog);
 
+    //Retrieves a paginated list of CompletedLogs.
     Page<CompletedLog> findAll(Pageable pageable);
 }
