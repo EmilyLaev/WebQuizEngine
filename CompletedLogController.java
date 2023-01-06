6813df0d87f9750a935ea92f516b403f1c27f2e1
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-06 18:24:03 -0500

    Update CompletedLogController.java

--- CompletedLogController.java
+++ CompletedLogController.java
@@ -10,12 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//Controller for handling HTTP requests related to CompletedLog entities.
 @RestController
 public class CompletedLogController {
 
     @Autowired
     private CompletedLogServices completedLogServices;
 
+    //This handles HTTP GET requests to retrieve a list of completed logs, sorted by the specified field.
+    //Default page is set to 0
     @GetMapping("/api/quizzes/completed")
     public Page<CompletedLog> findAll(
             @RequestParam(defaultValue = "0") Integer page,
