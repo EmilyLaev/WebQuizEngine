69cdc9349b094611c56e36468fab1bcfe101f92b
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-05 21:13:20 -0500

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -17,34 +17,44 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Quiz {
-
+    
+    // The unique identifier for the quiz
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    
+    // The title of the quiz
     @NotNull
     @NotBlank(message = "title is required")
     private String title;
 
+    // The text of the quiz
     @NotNull
     @NotBlank(message = "text is required")
     private String text;
+    
+    // The options for the quiz
     @NotNull
     @Size(min = 2)
     @ElementCollection
     private List<String> options;
 
+    // The correct solutions for the quiz
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ElementCollection
     private List<Integer> answer;
 
+    // The user that created the quiz
     @ManyToOne
     @JoinColumn(name = "user_id")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private User user;
 
+    // The date and time that the quiz was completed
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private LocalDateTime completedAt;
 
+    // Constructor for creating a new quiz
     public Quiz(String title, String text, List<String> options, List<Integer> answer) {
         this.title = title;
         this.text = text;
