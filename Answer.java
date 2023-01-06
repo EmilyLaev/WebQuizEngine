d1b2534d4ff44551f6c03f22ba37c09b2078dd02
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-06 14:50:29 -0500

    Update Answer.java

--- Answer.java
+++ Answer.java
@@ -3,11 +3,14 @@ package engine.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
+//This class represents the answers to the quiz questions
+//The answer are stored in the form of an integer list
 public class Answer {
 
     private List<Integer> answer;
