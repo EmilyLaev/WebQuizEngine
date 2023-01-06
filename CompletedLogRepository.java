62f7fa4c441fb6c4d5148a6375b05a02bc73b16e
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-06 18:36:43 -0500

    Update CompletedLogRepository.java

--- CompletedLogRepository.java
+++ CompletedLogRepository.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+//Repository for accessing CompletedLog entities from the database.
+//Retrieves a paginated list of CompletedLogs belonging to the user with the given ID.
 @Repository
 public interface CompletedLogRepository extends PagingAndSortingRepository<CompletedLog, Long> {
     Page<CompletedLog> findByUserId(Long id, Pageable pageable);
