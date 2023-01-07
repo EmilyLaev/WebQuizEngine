0343eb6de00d30f84c4ddaa5f89d91977ec780d5
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-06 19:29:37 -0500

    Update CustomUserDetails.java

--- CustomUserDetails.java
+++ CustomUserDetails.java
@@ -9,42 +9,52 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
+
+//Custom implementation of the UserDetails interface from Spring Security.
 @Getter
 @Setter
 public class CustomUserDetails implements UserDetails {
-
+    //The User entity represented by this UserDetails object
     private final User user;
 
+    //Constructs a CustomUserDetails object with the given User entity.
     public CustomUserDetails(User user) {
         super();
         this.user = user;
     }
 
+    //Returns the authorities (i.e. roles) of the user.
+    //@return a collection of GrantedAuthority objects representing the user's roles
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.singleton(new SimpleGrantedAuthority(user.getRole()));
     }
 
+    //Returns the password of the user.
     @Override
     public String getPassword() {
         return user.getPassword();
     }
 
+    //Returns the email of the user.
     @Override
     public String getUsername() {
         return user.getEmail();
     }
 
+    //Shows if user's account is not expired
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    //Shows that user's account is not locked
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    //Shows that user's credentials are not expired
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
@@ -55,6 +65,7 @@ public class CustomUserDetails implements UserDetails {
         return true;
     }
 
+    //Returns user's Id
     public Long getId() {
         return user.getId();
     }
