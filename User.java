71d245a3b90ad6d66610f969f56d2b5e87dd0632
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 11:47:19 -0500

    Update User.java

--- User.java
+++ User.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+//a class representing a user in the application.
 @Entity
 @Getter
 @Setter
@@ -19,15 +20,19 @@ import javax.validation.constraints.Size;
 @Builder
 public class User {
 
+    //The unique identifier of the user.
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    //The email address of the user
     @NotNull
     @Email(regexp = ".+@.+\\..+")
     private String email;
+    //The password of the user
     @NotNull
     @Size(min = 5)
     private String password;
+    //The role of the user (such as "admin", "moderator", etc.)
     private String role;
 
 }
