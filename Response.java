1eac54f60c8b6be7f15cd1219e72584075ec922f
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 11:36:02 -0500

    Update Response.java

--- Response.java
+++ Response.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//A class representing the response for the result
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Response {
+    //Indicates whether the solution was successful or not.
     private boolean success;
     private String feedback;
 }
