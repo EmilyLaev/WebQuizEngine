5abcaf1213c37f3d3089bdaca9d79f9bf1941701
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-06 19:52:54 -0500

    Update QuizService.java

--- QuizService.java
+++ QuizService.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+// Service interface for Quiz entity
+//Has the get, save and delete method for the quiz itself
 public interface QuizService {
 
     Quiz saveQuiz(Quiz quiz);
