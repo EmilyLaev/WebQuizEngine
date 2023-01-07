f4786ede40ad6dd61b9042cb2132c870b87fad72
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 11:56:03 -0500

    Update UserRespository.java

--- UserRespository.java
+++ UserRespository.java
@@ -4,8 +4,11 @@ import engine.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//A repository interface for the User entity.
+//Used to perform CRUD operations on User objects in the database.
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    //Retrieves a User object from the database by its email address
     User findByEmail(String email);
 
 }
