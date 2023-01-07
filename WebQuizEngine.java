703f688117de3a95ec24a7aee50228c93a5d350e
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 12:08:28 -0500

    Update WebQuizEngine.java

--- WebQuizEngine.java
+++ WebQuizEngine.java
@@ -3,9 +3,12 @@ package engine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//The main class of a Spring Boot application
 @SpringBootApplication
 public class WebQuizEngine {
 
+    //The entry point of the application.
+    //Launches the application, creating the beans and starting the embedded Tomcat server.
     public static void main(String[] args) {
         SpringApplication.run(WebQuizEngine.class, args);
     }
