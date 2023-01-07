84f5779ed887928a6e93cf6c69f4dfa2965808fc
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 11:58:44 -0500

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -2,6 +2,8 @@ package engine.services;
 
 import engine.entity.User;
 
+//A service interface for the User entity
+//Saves a User object to the database
 public interface UserService {
 
     void saveUser(User user);
