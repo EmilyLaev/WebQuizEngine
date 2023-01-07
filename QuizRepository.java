efe657dd27948f7f2c8a5f6d67e419834d2dbc08
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-06 19:48:31 -0500

    Update QuizRepository.java

--- QuizRepository.java
+++ QuizRepository.java
@@ -5,6 +5,9 @@ import engine.entity.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// Repository interface for Quiz entity
+// No need to define any additional methods, since the basic CRUD operations
+// (save, findById, delete, etc.) are already provided by JpaRepository.
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Integer> {
 }
