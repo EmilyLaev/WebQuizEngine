1985c8132b64b05643f5436d904457824edd433e
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-07 11:51:27 -0500

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -9,12 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+
+//A controller class responsible for handling HTTP requests related to users
 @RestController
 public class UserController {
 
+    //A service layer class containing business logic related to users
     @Autowired
     private UserService userService;
 
+    //Handles an HTTP POST request to the "/api/register" URL.
+    //Registers a new user by saving the user object passed in the request body.
+    //Returns an HTTP status code of 204 (No Content) when successful.
     @PostMapping("/api/register")
     public void registerUser(@Valid @RequestBody User user) {
         userService.saveUser(user);
