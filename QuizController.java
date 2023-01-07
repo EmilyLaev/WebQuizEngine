f64d0f1cdcb2fc617ca762e1cbb8eb8f155eb0f5
Emily Laevens <69812673+EmilyLaev@users.noreply.github.com>
2023-01-06 19:45:53 -0500

    Update QuizController.java

--- QuizController.java
+++ QuizController.java
@@ -13,39 +13,50 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+//REST controller for interacting with Quiz entities.
 @RestController
 public class QuizController {
 
+    //Service for interacting with Quiz entities
     @Autowired
     private QuizService quizService;
 
+    //Handles a POST request to the "/api/quizzes" endpoint.
+    //Saves the given Quiz object and returns it.
     @PostMapping("/api/quizzes")
     public Quiz sentQuiz(@Valid @RequestBody Quiz quiz) {
         return quizService.saveQuiz(quiz);
     }
 
+    //Handles a GET request to the "/api/quizzes" endpoint.
+    //Returns a paginated list of Quiz objects.
     @GetMapping("/api/quizzes")
     public Page<Quiz> findAll(@RequestParam(defaultValue = "0") Integer page) {
         PageRequest pageRequest = PageRequest.of(page, 10);
         return quizService.findAll(pageRequest);
     }
 
+    //Handles a GET request to the "/api/quizzes/{id}" endpoint.
+    //Returns the Quiz object with the given id.
     @GetMapping("/api/quizzes/{id}")
     public Quiz getById(@PathVariable("id") int id) {
         return quizService.getQuizById(id);
     }
 
+    //Handles the POST request to the  "/api/quizzes/{id}/solve" endpoint
     @PostMapping("/api/quizzes/{id}/solve")
     public Response getResponse(@PathVariable("id") int id, @RequestBody Answer answer) {
         return quizService.getAnswer(id, answer);
     }
 
+    //Handles a DELETE request to the "/api/quizzes/{id}" endpoint
     @DeleteMapping("/api/quizzes/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void deleteQ(@PathVariable("id") int id) {
         quizService.deleteQ(id);
     }
 
+    //Handles a GET request to the "/api/test" endpoint
     @GetMapping("/api/test")
     @ResponseBody
     public String test(Authentication authentication) {
